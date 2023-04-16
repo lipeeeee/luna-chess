83c9d4de8fa524807b85e7a8f84de984e3d8071d
lipeeeee <filipe.pereirafga@gmail.com>
2023-04-16 15:55:28 +0100

    src

---
+++ src/main.py
@@ -0,0 +1,17 @@
+"""
+    Zero Knowledge Chess Engine
+
+    by lipeeeee
+"""
+
+import chess as chess
+import sys
+
+def main() -> int:
+    """Entry Point"""
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
