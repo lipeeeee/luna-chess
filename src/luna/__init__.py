c5b3b6e44dea6bc9e9e3c66ef25f2b85012fb592
fgoncalves <fgoncalves@generixgroup.com>
2023-04-18 12:10:31 +0100

    no compile time errors lol, also luna_constants.py

--- src/luna/__init__.py
+++ src/luna/__init__.py
@@ -9,10 +9,6 @@ from .luna_eval import LunaEval
 from .luna_dataset import LunaDataset
 
 # Luna constants
-LUNA_MAIN_FOLDER = "C:\\Users\\fgoncalves\\dev\\luna"
-LUNA_DATA_FOLDER = "data"
-CURRENT_MODEL = "main_luna.pth"
-NUM_SAMPLES = 2_500_000
 from .luna_dataset import DATASET_FOLDER, DATASET_PREFIX 
 from .luna_eval import MAXVAL
 from .luna_NN import MODEL_FOLDER
--- src/luna/luna.py
+++ src/luna/luna.py
@@ -5,8 +5,7 @@
 import os
 import numpy as np
 import chess.pgn 
-import torch
-from luna import LunaDataset, LunaNN, LUNA_MAIN_FOLDER, CURRENT_MODEL
+from .luna_NN import LunaNN
 
 class Luna():
     """Luna_chess engine main class"""
@@ -14,11 +13,11 @@ class Luna():
     def __init__(self, verbose=False) -> None:
         """If on initialization there is no pre-saved model we create one and train it, to then save it"""
         self.verbose = verbose
-        self.luNNa = LunaNN(model_file=CURRENT_MODEL)
+        self.luNNa = LunaNN()
         
         # Check if model exists
         if self.luNNa.model_exists():
-            if verbose: print(f"[BUILDING] Loading model({CURRENT_MODEL})...")
+            if verbose: print(f"[BUILDING] Loading model({self.luNNa.model_file})...")
             self.luNNa.load()
             return
         
@@ -92,41 +91,7 @@ class Luna():
         return
 
     @staticmethod
-    def serialize_board(board: chess.Board):
-        """Serialize a chess board into a NN readable format
-            1. Encode board
-            2. Encode board into binary representation(4bit)
-            3. Encode turn
-        """
-        
-        # Check if valid board before preprocessing
-        assert board.is_valid()
-
-        # 1. Board state encoding 
-        board_state = np.zeros(64, np.uint8)
-        for i in range(64):
-            pp = board.piece_at(i)
-            
-            if pp is None:
-                continue
-            board_state[i] = {"P": 1, "N": 2, "B": 3, "R": 4, "Q": 5, "K": 6, \
-                    "p": 9, "n":10, "b":11, "r":12, "q":13, "k": 14}[pp.symbol()]
-        
-        board_state.reshape(8, 8)
-
-        # 2. Binary state
-        state = np.zeros((5, 8, 8), np.uint8)
-
-        # 0 - 3 columns to binary
-        state[0] = (board_state>>3)&1
-        state[1] = (board_state>>2)&1
-        state[2] = (board_state>>1)&1
-        state[3] = (board_state>>0)&1
-
-        # 4th column is who's turn it is
-        state[4] = (board.turn*1.0)
-
-        return state
+    
 
     def old_preprocess_game(self, game: chess.pgn.Game):
         """Preprocess game into a vector with:
